b7f98ff6615752a244d14c0a7f59ee7fb11b564f
张建飞 <jianfei.zhang@weicheche.cn>
2019-08-27 09:25:05 +0800

    Update: add leetcode module

---
+++ leetcode/src/L206.java
@@ -0,0 +1,26 @@
+public class L206 {
+
+    public ListNode reverseList (ListNode head) {
+        if (head == null) return null;
+
+        //引入哨兵节点(该节点next永远指向head，最后返回next节点即可)
+        ListNode soldier = new ListNode(8080);
+        ListNode p = head;
+
+        while (p != null) {
+            ListNode pNext = p.next;
+            p.next = soldier.next;
+            soldier.next = p;
+            p = pNext;
+        }
+
+        return soldier.next;
+    }
+
+    static public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode (int x) { val = x; }
+    }
+}
