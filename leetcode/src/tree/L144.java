cc39543a881159a10e26c4c591bba8e79b747c93
张建飞 <jianfei.zhang@weicheche.cn>
2019-08-28 10:45:38 +0800

    Update: L144 Binary Tree Preorder Traversal

---
+++ leetcode/src/tree/L144.java
@@ -0,0 +1,42 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,2,3]
+ */
+public class L144 {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        TreeNode p = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (p != null || !stack.isEmpty()) {
+            while (p != null) {
+                list.add(p.val);
+                if (p.right != null) stack.push(p.right);
+                p = p.left;
+            }
+            if (!stack.isEmpty()) p = stack.pop();
+        }
+
+        return list;
+    }
+}
