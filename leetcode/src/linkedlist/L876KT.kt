83bb1322da78e3d18c9bec6d77068973d361933f
zhangjianfei <zhangjianfei@coocaa.com>
2020-10-06 21:23:22 +0800

    Add L876KT
    Change-Id: Iaaff5e3db08e9b55256f5f1ae62dde058d7c187a

---
+++ leetcode/src/linkedlist/L876KT.kt
@@ -0,0 +1,33 @@
+package linkedlist
+
+/**
+ * 876. Middle of the Linked List
+ *
+ * Given a non-empty, singly linked list with head node head,
+ * return a middle node of linked list.
+ * If there are two middle nodes, return the second middle node.
+ */
+
+fun main() {
+//    val node = NodeUtils.arrayToList(1, 2, 6, 3, 4, 5, 1, 2, 6, 3, 4, 5)
+//    val node = NodeUtils.arrayToList(1, 2, 6, 3, 4, 5)
+    val node = NodeUtils.arrayToList(1, 2, 3)
+//    val node = NodeUtils.arrayToList(1, 2)
+//    val node = NodeUtils.arrayToList(1)
+    NodeUtils.printList(middleNode(node))
+}
+
+fun middleNode(head: ListNode?): ListNode? {
+    if (head == null) return null
+
+    var fast = head
+    var slow = head
+
+    while (fast?.next != null && fast.next.next != null) {
+        fast = fast.next.next
+        slow = slow?.next
+    }
+
+    return if (fast?.next == null) slow
+    else slow?.next
+}
