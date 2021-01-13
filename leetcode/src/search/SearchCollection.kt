f7825ba3205954ceb94fc20504efa7c9dfe3ff0e
zhangjianfei <zhangjianfei@coocaa.com>
2021-01-14 00:31:43 +0800

    add ext1 ext2 of binary search
    Change-Id: I68f90b537ac1d6e51cbce4b3a126d7d23e4f3a29

--- leetcode/src/search/SearchCollection.kt
+++ leetcode/src/search/SearchCollection.kt
@@ -2,7 +2,11 @@ package search
 
 fun main() {
     val count = 10000
-    val array = Array(count){it}
+    val arrIncrease = Array(count) { it }
+    val arrDuplicate = Array(count) { 1 }
+//    val search = ::search0
+//    val search = ::bSearchExt01
+    val search = ::bSearchExt02
 
     val value0 = -100
     val value1 = 1001
@@ -11,17 +15,19 @@ fun main() {
     val value4 = 999
     val value5 = 1000
     val value6 = 555
+    val value7 = 1
 
-    testSearch(value0, array, ::search0)
-    testSearch(value1, array, ::search0)
-    testSearch(value2, array, ::search0)
-    testSearch(value3, array, ::search0)
-    testSearch(value4, array, ::search0)
-    testSearch(value5, array, ::search0)
-    testSearch(value6, array, ::search0)
-
+    testSearch(value0, arrIncrease, search)
+    testSearch(value1, arrIncrease, search)
+    testSearch(value2, arrIncrease, search)
+    testSearch(value3, arrIncrease, search)
+    testSearch(value4, arrIncrease, search)
+    testSearch(value5, arrIncrease, search)
+    testSearch(value6, arrIncrease, search)
+    testSearch(value7, arrDuplicate, search)
 
 }
+
 fun testSearch(value: Int, array: Array<Int>, search: (Int, Array<Int>) -> Int) {
     println("search value: $value")
     val result = search.invoke(value, array)
@@ -40,6 +46,60 @@ fun search0(value: Int, array: Array<Int>): Int {
         if (value == array[middle]) return middle
         if (value < array[middle]) high = middle - 1
         else low = middle + 1
+    }
+
+    return -1
+}
+
+/**
+ * binary search ext01:
+ * return 1st index when the value equals target
+ * if null return -1
+ */
+fun bSearchExt01(target: Int, arr: Array<Int>): Int {
+    var low = 0
+    var high = arr.size - 1
+
+    while (low <= high) {
+        val mid = low + ((high - low) shr 1)
+        when {
+            target > arr[mid] -> {
+                low = mid + 1
+            }
+            target < arr[mid] -> {
+                high = mid - 1
+            }
+            else -> {
+                if (mid == 0 || arr[mid - 1] != target) return mid
+                else high = mid - 1
+            }
+        }
+    }
+
+    return -1
+}
+
+/**
+ * binary search ext02:
+ * return last index when the value equals target
+ * if null return -1
+ */
+// TODO boundary
+fun bSearchExt02(target: Int, arr: Array<Int>): Int {
+    var lo = 0
+    var hi = arr.size - 1
+
+    while (lo <= hi) {
+        val mid = lo + ((hi - lo) shr 1)
+
+        when {
+            target < arr[mid] -> hi = mid - 1
+            target > arr[mid] -> lo = mid + 1
+            else -> {
+                if (mid == arr.size - 1 || arr[mid + 1] != target) return mid
+                else lo = mid + 1
+            }
+        }
     }
 
     return -1
