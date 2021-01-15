217f494ae78785485a723c593d6d4aa99699a175
zhangjianfei <zhangjianfei@coocaa.com>
2021-01-16 00:11:35 +0800

    add preorder traversal of binary tree in java copy
    Change-Id: If80e2fed31980b42edfb0bfccbc64c767ddb5862

---
+++ leetcode/src/tree/L144Copy.java
@@ -0,0 +1,52 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * Input: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * Output: [1,2,3]
+ */
+public class L144Copy {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        TreeNode p = root;
+        Stack<TreeNode> s = new Stack<>();
+
+        while (p != null || !s.isEmpty()) {
+            while (p != null) {
+                list.add(p.val);
+                if (p.right != null) s.push(p.right);
+                p = p.left;
+            }
+
+            if (!s.isEmpty()) p = s.pop();
+        }
+
+        return list;
+    }
+
+    public static void main (String[] args) {
+//        TreeNode node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4);
+        TreeNode node = TreeUtils.array2Tree(3, 1, 5, 6, 3, 0, 7);
+
+
+        //        TreeUtils.printTree(node);
+        List<Integer> list = new L144Copy().preorderTraversal(node);
+        for (int i : list) System.out.print(i + " ");
+    }
+}
