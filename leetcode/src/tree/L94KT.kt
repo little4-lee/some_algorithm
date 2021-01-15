94d9ce8b4d254f464bce12ade70469f2e3e157eb
zhangjianfei <zhangjianfei@coocaa.com>
2021-01-15 23:48:45 +0800

    add inorder traversal of binary tree in kotlin
    Change-Id: I802d03b817e390462089f8db135b3140661b1924

---
+++ leetcode/src/tree/L94KT.kt
@@ -0,0 +1,36 @@
+package tree
+
+import java.util.*
+
+fun inorderTraversal(root: TreeNode?):  List<Int> {
+    val list = mutableListOf<Int>()
+    var p = root
+    val stack = Stack<TreeNode>()
+
+    while (p != null || stack.isNotEmpty()) {
+        while (p != null) {
+            stack.push(p)
+            p = p.left
+        }
+
+        if (stack.isNotEmpty()) {
+            p = stack.pop()
+            list.add(p.`val`)
+            p = p.right
+        }
+    }
+
+    return list
+}
+
+fun main() {
+    val node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4)
+
+
+//        TreeUtils.printTree(node);
+
+
+//        TreeUtils.printTree(node);
+    val list = inorderTraversal(node)
+    for (i in list) print("$i ")
+}
