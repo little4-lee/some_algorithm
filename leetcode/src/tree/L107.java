1bd72be7db391d6ad147d2d0c974f3f5dbe6f669
zhangjianfei <zhangjianfei@coocaa.com>
2021-01-16 20:06:58 +0800

    add L107 in non-recursive
    Change-Id: I76d7b987958cbe27c302af6ce708e2f7531b6926

--- leetcode/src/tree/L107.java
+++ leetcode/src/tree/L107.java
@@ -3,6 +3,8 @@ package tree;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 107. Binary Tree Level Order Traversal II
@@ -26,24 +28,55 @@ import java.util.List;
  */
 public class L107 {
     //recursive
+//    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//        List<List<Integer>> list = new LinkedList<>();
+//        levelOrderBottomInternal(root, 0, list);
+//        Collections.reverse(list);
+//        return list;
+//    }
+//
+//    private void levelOrderBottomInternal (TreeNode root, int depth, List<List<Integer>> list) {
+//        if (root == null) return;
+//
+//        if (depth >= list.size()) list.add(depth, new LinkedList<>());
+//
+//        List<Integer> inner = list.get(depth);
+//        inner.add(root.val);
+//
+//        levelOrderBottomInternal(root.left, depth + 1, list);
+//        levelOrderBottomInternal(root.right, depth + 1, list);
+//    }
+
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new LinkedList<>();
+
         List<List<Integer>> list = new LinkedList<>();
-        levelOrderBottomInternal(root, 0, list);
+
+        Queue<TreeNode> queue = new LinkedBlockingQueue<>();
+        queue.offer(root);
+
+        int cur, count;
+
+        while (!queue.isEmpty()) {
+            cur = 0;
+            count = queue.size();
+            List<Integer> inner = new LinkedList<>();
+            list.add(inner);
+
+            while (cur < count) {
+                cur++;
+                TreeNode p = queue.poll();
+                inner.add(p.val);
+                if (p.left != null) queue.offer(p.left);
+                if (p.right != null) queue.offer(p.right);
+            }
+        }
+
         Collections.reverse(list);
+
         return list;
     }
 
-    private void levelOrderBottomInternal (TreeNode root, int depth, List<List<Integer>> list) {
-        if (root == null) return;
-
-        if (depth >= list.size()) list.add(depth, new LinkedList<>());
-
-        List<Integer> inner = list.get(depth);
-        inner.add(root.val);
-
-        levelOrderBottomInternal(root.left, depth + 1, list);
-        levelOrderBottomInternal(root.right, depth + 1, list);
-    }
 
     public static void main (String[] args) {
         TreeNode node = TreeUtils.array2Tree(3, 2, 7, 4, null, 5, 6);
