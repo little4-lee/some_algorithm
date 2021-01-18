08d4492ee7bbacd107b930f1e5d0e59105ee631c
zhangjianfei <zhangjianfei@coocaa.com>
2021-01-19 00:18:33 +0800

    add L142 detect cycle and return cycled node
    Change-Id: I8c55b33bac5dfa3a218e72a4dfb5423a52a874e7

---
+++ leetcode/src/linkedlist/L142KT.kt
@@ -0,0 +1,33 @@
+package linkedlist
+
+class L142KT {
+    fun detectCycle(head: ListNode?): ListNode? {
+        head ?: return null
+        head.next ?: return null
+
+        var slow = head.next
+        var fast = head.next.next
+        //detect cycle
+        while (fast?.next != null) {
+            if (slow == fast) break
+            slow = slow.next
+            fast = fast.next.next
+        }
+
+        if (slow != fast) return null
+
+        //get cycle node
+        var p = head
+        while (p != slow) {
+            p = p?.next
+            slow = slow.next
+        }
+        return p
+    }
+}
+
+fun main() {
+    val list = arrayToList(1, 2, 3, 4, 5, 6, 7)
+    NodeUtils.addCycle(list, 1)
+    println(L142KT().detectCycle(list)?.`val`)
+}
