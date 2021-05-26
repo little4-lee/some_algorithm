c90bb25cf5f2aefbf981bb7bf38560f08de1730d
zhangjianfei <zhangjianfei@coocaa.com>
2021-05-27 00:03:48 +0800

    Add L19 remove nth node from end of list
    Change-Id: Id20438d7c8865e7656b364d307e19a28b30a990a

---
+++ leetcode/src/linkedlist/l19_remove_nth_node_from_end_of_list/L19.java
@@ -0,0 +1,65 @@
+package linkedlist.l19_remove_nth_node_from_end_of_list;
+
+import linkedlist.ListNode;
+import linkedlist.NodeUtils;
+
+/**
+ * 19. Remove Nth Node From End of List
+ * <p>
+ * Given the head of a linked list,
+ * remove the nth node from the end of the list and return its head.
+ * Follow up: Could you do this in one pass?
+ * <p>
+ * Example 1:
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * <p>
+ * Example 2:
+ * Input: head = [1], n = 1
+ * Output: []
+ * <p>
+ * Example 3:
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ */
+public class L19 {
+    public ListNode removeNthFromEnd (ListNode head, int n) {
+        if (head == null) return null;
+
+        ListNode soldier = new ListNode(0);
+        soldier.next = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        int index = 1;
+        while (index < n && fast != null) {
+            index++;
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            //list size < n
+            return head;
+        }
+
+        ListNode slowPre = soldier;
+        while (fast.next != null) {
+            fast = fast.next;
+            slowPre = slow;
+            slow = slow.next;
+        }
+
+        slowPre.next = slow.next;
+        slow.next = null;
+
+        return soldier.next;
+    }
+
+    public static void main (String[] args) {
+        L19 l = new L19();
+//        ListNode list = NodeUtils.arrayToList(1, 2, 3, 4, 5);
+        ListNode list = NodeUtils.arrayToList(1, 2);
+//        NodeUtils.printList(l.removeNthFromEnd(list, 2));
+        NodeUtils.printList(l.removeNthFromEnd(list, 1));
+    }
+}
--- leetcode/src/linkedlist/l19_remove_nth_node_from_end_of_list/L19KT.kt
+++ leetcode/src/linkedlist/l19_remove_nth_node_from_end_of_list/L19KT.kt
@@ -1,6 +1,8 @@
 package linkedlist
 
 /**
+ * 19. Remove Nth Node From End of List
+ *
  * Given the head of a linked list,
  * remove the nth node from the end of the list and return its head.
  * Follow up: Could you do this in one pass?
@@ -28,34 +30,34 @@ fun main() {
 //    printList(removeNthFromEnd(arrayToList(1), 1))
 }
 
-fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-    head ?: return head
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        head ?: return head
 
-    val soldier = ListNode(0)
-    soldier.next = head
+        val soldier = ListNode(0)
+        soldier.next = head
 
-    var index = 1
-    var fast = soldier.next
-    var slow = soldier
-    while (index < n && fast.next != null) {
-        fast = fast.next
-        index++
-    }
+        var index = 1
+        var fast = soldier.next
+        var slow = soldier
+        while (index < n && fast.next != null) {
+            fast = fast.next
+            index++
+        }
 
-    if (index < n) {
-        //链表长度小于n
+        if (index < n) {
+            //链表长度小于n
+            return soldier.next
+        }
+
+        while (fast.next != null) {
+            fast = fast.next
+            slow = slow.next
+        }
+
+        //remove node: slow.next
+        val temp = slow.next
+        slow.next = slow.next.next
+        temp.next = null
+
         return soldier.next
     }
-
-    while (fast.next != null) {
-        fast = fast.next
-        slow = slow.next
-    }
-
-    //remove node: slow.next
-    val temp = slow.next
-    slow.next = slow.next.next
-    temp.next = null
-
-    return soldier.next
-}
