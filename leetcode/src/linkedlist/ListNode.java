159c3f6eade39446221e83c768bb6e39f54f0a04
zhangjianfei <zhangjianfei@coocaa.com>
2021-05-26 21:30:30 +0800

    Update ListNode filed accecss
    Change-Id: I6a8e9e71057b9941c10212135086eab8b4ac1438

--- leetcode/src/linkedlist/ListNode.java
+++ leetcode/src/linkedlist/ListNode.java
@@ -1,8 +1,7 @@
 package linkedlist;
 
 public class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode (int x) { val = x; }
+    public int val;
+    public ListNode next;
+    public ListNode (int x) { val = x; }
 }
