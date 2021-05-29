7307422d1466f395fb677f8cf2d9551b317b1ef9
zhangjianfei <zhangjianfei@coocaa.com>
2021-05-29 15:42:59 +0800

    Add L160 intersection of two linked list
    Change-Id: I0d23f62a22c14f2d513f68330e27d0cd19ca77a8

--- leetcode/src/linkedlist/l160_intersection_of_two_linked_lists/L160.java
+++ leetcode/src/linkedlist/l160_intersection_of_two_linked_lists/L160.java
@@ -1,5 +1,19 @@
-package linkedlist;
+package linkedlist.l160_intersection_of_two_linked_lists;
 
+import linkedlist.ListNode;
+import linkedlist.NodeUtils;
+
+/**
+ * 160. Intersection of Two Linked Lists
+ * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+ *
+ * For example, the following two linked lists begin to intersect at node c1:
+ *
+ *
+ * It is guaranteed that there are no cycles anywhere in the entire linked structure.
+ *
+ * Note that the linked lists must retain their original structure after the function returns.
+ */
 public class L160 {
     public ListNode getIntersectionNode (ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
