fa6cbf7f0cc7abd09bcbde3e464607841c7e3fcb
zhangjianfei <zhangjianfei@coocaa.com>
2021-05-30 17:16:22 +0800

    Add binary search ext2 find last equals target
    Change-Id: I9f5e2b162a191259995fa6b4cb31b2a7e82af5d1

--- leetcode/src/search/binary_search/SearchCollectionCopy.java
+++ leetcode/src/search/binary_search/SearchCollectionCopy.java
@@ -25,17 +25,17 @@ public class SearchCollectionCopy {
         //        System.out.println(collection.bSearch(arr4, target));
         //        System.out.println(collection.bSearch(arr5, target));
 
-        System.out.println(collection.bSearchExt1(arr1, target));
-        System.out.println(collection.bSearchExt1(arr2, target));
-        System.out.println(collection.bSearchExt1(arr3, target));
-        System.out.println(collection.bSearchExt1(arr4, target));
-        System.out.println(collection.bSearchExt1(arr5, target));
+//        System.out.println(collection.bSearchExt1(arr1, target));
+//        System.out.println(collection.bSearchExt1(arr2, target));
+//        System.out.println(collection.bSearchExt1(arr3, target));
+//        System.out.println(collection.bSearchExt1(arr4, target));
+//        System.out.println(collection.bSearchExt1(arr5, target));
 
-        //        System.out.println(collection.bSearchExt2(arr1, target));
-        //        System.out.println(collection.bSearchExt2(arr2, target));
-        //        System.out.println(collection.bSearchExt2(arr3, target));
-        //        System.out.println(collection.bSearchExt2(arr4, target));
-        //        System.out.println(collection.bSearchExt2(arr5, target));
+                System.out.println(collection.bSearchExt2(arr1, target));
+                System.out.println(collection.bSearchExt2(arr2, target));
+                System.out.println(collection.bSearchExt2(arr3, target));
+                System.out.println(collection.bSearchExt2(arr4, target));
+                System.out.println(collection.bSearchExt2(arr5, target));
 
         //        System.out.println(collection.bSearchExt3(arr1, target));
         //        System.out.println(collection.bSearchExt3(arr2, target));
@@ -107,7 +107,22 @@ public class SearchCollectionCopy {
      * @return
      */
     public int bSearchExt2 (int[] arr, int target) {
-        // TODO: 2021/5/30
+        if (arr == null) return -1;
+
+        int lo = 0;
+        int hi = arr.length - 1;
+        int lastIndex = hi;
+
+        while (lo <= hi) {
+            int middle = lo + ((hi - lo) >> 1);
+            if (arr[middle] == target) {
+                if (middle == lastIndex || arr[middle + 1] != target) return middle;
+                else lo = middle + 1;
+            } else if (arr[middle] < target) lo = middle + 1;
+            else hi = middle - 1;
+
+        }
+
         return -1;
     }
 
