dca721ac71f6df07f2766afdfe94a6a27c1a917a
littlefourthlee <watermydream@gmail.com>
2022-11-22 11:15:22 +0800

    Add tree submission: binary tree traversal in-order

---
+++ src/main/java/leetcode/tree/l94_binary_tree_inorder_traversal/L94Copy3.java
@@ -0,0 +1,56 @@
+package leetcode.tree.l94_binary_tree_inorder_traversal;
+
+import common.TreeNode;
+import common.TreeUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Binary Tree Inorder Traversal
+ * <p>
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [1,3,2]
+ */
+public class L94Copy3 {
+    public List<Integer> inorderTraversal (TreeNode root) {
+        if (root == null) return null;
+
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+
+        while (p != null || !stack.isEmpty()) {
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+            if (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                p = node.right;
+            }
+        }
+
+        return list;
+    }
+
+    public static void main (String[] args) {
+        TreeNode node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4);
+//        TreeNode node = TreeUtils.array2Tree(1, 2, 3, 4, null, 6, null, null, 9);
+
+
+//        TreeUtils.printTree(node);
+        List<Integer> list = new L94Copy3().inorderTraversal(node);
+        for (int i : list) System.out.print(i + " ");
+    }
+}
