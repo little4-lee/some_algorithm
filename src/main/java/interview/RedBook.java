57fad1a43ca1d694cee1c23e8f46424378572232
littlefourthlee <watermydream@gmail.com>
2023-01-19 18:22:17 +0800

    link answer to redbook interview question
    Change-Id: I05f2fb82b4bc435c359d423fad740ea57c90a84a

--- src/main/java/interview/RedBook.java
+++ src/main/java/interview/RedBook.java
@@ -3,6 +3,7 @@ package interview;
 * @ClassName: RedBook
 * @Author: elon
 * @CreateDate: 2022/12/13 16:19
-* @Description: 
+* @Description:
+ * @see leetcode.num._42_trapping_rain_water
 */   class RedBook   {
 }
