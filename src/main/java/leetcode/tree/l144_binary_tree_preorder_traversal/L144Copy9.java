34fd773e632a3eb830d1829af5dfa0bf44e8e2a2
littlefourthlee <watermydream@gmail.com>
2026-08-06 16:10:40 +0800

    L144 preorder copy9

---
+++ src/main/java/leetcode/tree/l144_binary_tree_preorder_traversal/L144Copy9.java
@@ -0,0 +1,56 @@
+package leetcode.tree.l144_binary_tree_preorder_traversal;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import common.TreeNode;
+import common.TreeUtils;
+
+/**
+ * Binary Tree Preorder Traversal
+ * <p>
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ * <p>
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * <p>
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [1,2,3]
+ */
+public class L144Copy9 {
+
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                list.add(cur.val);
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                cur = cur.left;
+            }
+            if (!stack.isEmpty()) {
+                cur = stack.pop();
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4);
+//        TreeNode node = TreeUtils.array2Tree(3, 1, 5, 6, 3, 0, 7);
+
+
+        //        TreeUtils.printTree(node);
+        List<Integer> list = new L144Copy9().preOrderTraversal(node);
+        for (int i : list) System.out.print(i + " ");
+    }
+}
