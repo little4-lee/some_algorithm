f99f5d76423f59c1b1bfe0c79fb1a027e9778632
littlefourthlee <watermydream@gmail.com>
2026-08-07 11:11:24 +0800

    L94 binary tree in order traversal copy10

---
+++ src/main/java/leetcode/tree/l94_binary_tree_inorder_traversal/L94Copy10.java
@@ -0,0 +1,57 @@
+package leetcode.tree.l94_binary_tree_inorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import common.TreeNode;
+import common.TreeUtils;
+
+/**
+ * Binary Tree Inorder Traversal
+ * <p>
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [1,3,2]
+ */
+public class L94Copy10 {
+    public List<Integer> inorderTraversal (TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (cur != null || !stack.isEmpty()) {
+            // 找到最左边的子节点
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (!stack.isEmpty()) {
+                TreeNode temp = stack.pop();
+                list.add(temp.val);
+                if (temp.right != null) {
+                    cur = temp.right;
+                }
+            }
+        }
+
+        return list;
+    }
+
+    public static void main (String[] args) {
+        TreeNode node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4);
+//        TreeNode node = TreeUtils.array2Tree(1, 2, 3, 4, null, 6, null, null, 9);
+
+
+//        TreeUtils.printTree(node);
+        List<Integer> list = new L94Copy10().inorderTraversal(node);
+        for (int i : list) System.out.print(i + " ");
+    }
+}
