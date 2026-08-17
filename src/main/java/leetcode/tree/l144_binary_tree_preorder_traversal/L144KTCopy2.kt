24062dd779fc5baba4d909b43eba5432acf287e2
littlefourthlee <watermydream@gmail.com>
2026-08-17 13:18:18 +0800

    完善二叉树前序遍历实现

--- src/main/java/leetcode/tree/l144_binary_tree_preorder_traversal/L144KTCopy2.kt
+++ src/main/java/leetcode/tree/l144_binary_tree_preorder_traversal/L144KTCopy2.kt
@@ -5,15 +5,28 @@ import common.TreeNode
 import common.TreeUtils
 import java.util.*
 
-class L144KTCopy1 {
+class L144KTCopy2 {
     fun preorderTraversal(root: TreeNode?): List<Int>? {
+        //recursive
+//        root ?: return null
+//        val list = mutableListOf<Int>()
+//        list.add(root.`val`)
+//        preorderTraversal(root.left)?.let {
+//            list.addAll(it)
+//        }
+//        preorderTraversal(root.right)?.let {
+//            list.addAll(it)
+//        }
+//        return list
+//
+        // non recursive
         root ?: return null
-
         val list = mutableListOf<Int>()
         val stack = Stack<TreeNode>()
         var cur = root
 
         while (cur != null || stack.isNotEmpty()) {
+            // 找到最左边节点
             while (cur != null) {
                 list.add(cur.`val`)
                 if (cur.right != null) {
@@ -22,8 +35,7 @@ class L144KTCopy1 {
                 cur = cur.left
             }
             if (stack.isNotEmpty()) {
-                val tempNode = stack.pop()
-                cur = tempNode
+                cur = stack.pop()
             }
         }
 
@@ -32,8 +44,8 @@ class L144KTCopy1 {
 }
 
 fun main() {
-    printList(L144KTCopy1().preorderTraversal(TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4)))
-    printList(L144KTCopy1().preorderTraversal(TreeUtils.array2Tree(null)))
-    printList(L144KTCopy1().preorderTraversal(null))
+    printList(L144KTCopy2().preorderTraversal(TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4)))
+    printList(L144KTCopy2().preorderTraversal(TreeUtils.array2Tree(null)))
+    printList(L144KTCopy2().preorderTraversal(null))
 //    for (i in list) print("$i ")
 }
