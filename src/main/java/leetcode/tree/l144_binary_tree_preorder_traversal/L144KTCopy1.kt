81884f1c470bdd5615bc0f28663d61aaef4af254
littlefourthlee <watermydream@gmail.com>
2026-08-17 13:00:31 +0800

    新增二叉树前序遍历实现

---
+++ src/main/java/leetcode/tree/l144_binary_tree_preorder_traversal/L144KTCopy1.kt
@@ -0,0 +1,33 @@
+package leetcode.tree.l144_binary_tree_preorder_traversal
+
+import common.TreeNode
+import common.TreeUtils
+import java.util.*
+
+class L144KTCopy1 {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        val list = mutableListOf<Int>()
+        var p = root
+        val s = Stack<TreeNode>()
+
+        while (p != null || s.isNotEmpty()) {
+            while (p != null) {
+                list.add(p.`val`)
+                p.right?.let { s.push(it) }
+                p = p.left
+            }
+            if (s.isNotEmpty()) p = s.pop()
+        }
+        return list
+    }
+}
+
+fun main() {
+    val node = TreeUtils.array2Tree(3, 6, 7, 5, 0, 2, 8, 0, 0, 0, 0, 4)
+//    val node = TreeUtils.array2Tree(3, 1, 5, 6, 3, 0, 7)
+//        TreeUtils.printTree(node);
+
+//        TreeUtils.printTree(node);
+    val list = L144KTCopy1().preorderTraversal(node)
+    for (i in list) print("$i ")
+}
