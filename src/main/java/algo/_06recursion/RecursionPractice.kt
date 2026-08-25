99b1d1e68bf3740cd42f6627a511a0a4d47f6322
zhangjianfei03 <zhangjianfei03@baidu.com>
2026-08-25 10:23:12 +0800

    新增非递归爬楼梯实现

--- src/main/java/algo/_06recursion/RecursionPractice.kt
+++ src/main/java/algo/_06recursion/RecursionPractice.kt
@@ -1,5 +1,7 @@
 package algo._06recursion
 
+import javax.imageio.metadata.IIOMetadataFormatImpl
+
 class RecursionPractice {
 
     fun findLine(line: Int) : Int{
@@ -16,5 +18,28 @@ class RecursionPractice {
         return upstartsWay(level - 1) + upstartsWay(level - 2)
     }
 
+    /**
+     * 非递归实现爬楼梯
+     * 逻辑：用循环模拟入栈出栈
+     */
+    fun upstartsWayNonRecursive(level: Int): Int {
+        if (level <= 0) return 0
+        if (level == 1) return 1
+        if (level == 2) return 2
+        var ret = 0
+        var pre = 2
+        var prepre = 1
+        for (i in 3 .. level) {
+            ret = pre + prepre
+            prepre = pre
+            pre = ret
+        }
+        return ret
+    }
+}
 
+fun main() {
+    val recursiveSample = RecursionPractice()
+    println(recursiveSample.upstartsWayNonRecursive(50))
+    println(recursiveSample.upstartsWay(50))
 }
